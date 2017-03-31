3469e2ecf3976f7176ae49c3ca74fceb57acdbc9
Jeremy Gaerke <jeremy.gaerke@donotemailme.com>
2017-03-31 05:25:14 -0400

    fixing deploy script

--- etc/scripts/deploy.sh
+++ etc/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-aws s3 sync $1 s3://cng-accounts-dev/ --delete --region us-east-2 \
+aws s3 sync $1 s3://ng2-cognito-app-dev/ --delete --region us-east-2 \
 --exclude "*.html"  \
 --metadata-directive REPLACE --expires 2100-01-01T00:00:00Z --cache-control max-age=2592000,public
 
-aws s3 sync $1 s3://cng-accounts-dev/ --delete --region us-east-2 \
+aws s3 sync $1 s3://ng2-cognito-app-dev/ --delete --region us-east-2 \
 --exclude "*" --include "*.html" \
 --metadata-directive REPLACE --cache-control max-age=60,public
 
